acd1029f713173f913e92c34fc76caff7b66b63c
Chris McKenzie <kristopolous@yahoo.com>
2012-01-17 01:56:50 -0800

    even more liberal mobile support

--- emptyv.js
+++ emptyv.js
@@ -160,7 +160,7 @@ function findOffset() {
 
     // If we have drifted more than xxx seconds from our destination offset
     // then we will shift forward
-  } else if ( lapse - _player[_active].getCurrentTime() > 13) {
+  } else if ( lapse - _player[_active].getCurrentTime() > 18) {
 
     // This is also the opportunity to see if we are laggy.
     // We give our lagCounter 2 points here (and we always take
@@ -177,7 +177,7 @@ function findOffset() {
 
     // We don't trust seeking to be insanely accurate so we throw an offset
     // on to it to avoid some kind of weird seeking loop.
-    _player[_active].seekTo(lapse + YTLOADTIME_sec * 5 / 3);
+    _player[_active].seekTo(lapse + YTLOADTIME_sec * 7 / 3);
   } 
 
   _lagCounter--;
@@ -199,7 +199,7 @@ function onYouTubePlayerReady(playerId) {
 
   if(++_loaded == 2) {
     findOffset();
-    setInterval(findOffset, PRELOAD_ms + 500);
+    setInterval(findOffset, PRELOAD_ms * 2);
     setInterval(updateytplayer, 250);
     _player[_next].setVolume(100);
   }
