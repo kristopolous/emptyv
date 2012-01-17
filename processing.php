6a76fa6000a4bbdaf49215402aeaa18b48a1392f
Chris McKenzie <kristopolous@yahoo.com>
2012-01-17 14:17:08 -0800

    better processing mail

--- processing.php
+++ processing.php
@@ -1,1 +1,1 @@
-<? mail("chris+mt80s@qaa.ath.cx", $_GET['type'], yaml_emit($_POST)); ?>
+<? mail("chris+mt80s@qaa.ath.cx", $_GET['type'] . ': ' . implode(',', array_values($_POST)), print_r($_POST, true)); ?>
