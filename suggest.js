bb09ec1ec046205d101866ef81b2cf6167885eff
Chris McKenzie <kristopolous@yahoo.com>
2012-01-17 20:20:03 -0800

    searching on the suggestion page

--- suggest.js
+++ suggest.js
@@ -1,4 +1,49 @@
 var 
   mydb = DB()
-    .insert(DB.objectify(["ytid", "length", "start", "end", "volume", "year", "artist", "title"], _duration))
-    .insert(DB.objectify(["ytid", "year", "artist", "title", "reason"], _bad));
+    .insert(DB.objectify(["id", "length", "start", "end", "volume", "year", "artist", "title"], _duration));
+//    .insert(DB.objectify(["ytid", "year", "artist", "title", "reason"], _bad));
+//
+function search(){
+  var 
+    rows = [],
+    artist = $("#artist").val(),
+    title = $("#title").val();
+
+  if(artist.length > 1 || title.length > 1) {
+    mydb.find({
+      artist: mydb.like(artist),
+      title: mydb.like(title),
+    }).select('artist','title','id').sort('artist').each(function(res){
+      rows.push([
+        '<td>' + res[0] + '</td>',
+        '<td>',
+          '<a target=_blank href=http://youtube.com/watch?v=' + res[2] + '>',
+            res[1],
+          '</a>',
+        '</td>'
+      ].join(''));
+      
+    });
+    $("#results").html( [
+        '<table>',
+         '<thead>',
+           '<tr>',
+             '<th>Artist</th>',
+             '<th>Title</th>',
+           '</tr>',
+         '</thead>',
+         '<tbody>',
+           '<tr>',
+              rows.join('</tr><tr>'),
+           '</tr>',
+         '</tbody>',
+       '</table>'
+     ].join(''));
+  } else {
+    $("#results").html( 'more data needed' );
+  }
+}
+
+$(function(){
+  $("#artist, #title").keyup(search);
+});
