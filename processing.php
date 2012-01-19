936da4aede927fb70a6d42c577eefb2c69783e05
Chris McKenzie <kristopolous@yahoo.com>
2012-01-18 22:50:58 -0800

    Fixing the suggestion page

--- processing.php
+++ processing.php
@@ -1,4 +1,6 @@
 <? mail("mt80s@qaa.ath.cx", $_GET['type'] . ': ' . implode(',', array_values($_POST)), print_r($_POST, true)); ?>
 <link rel=stylesheet href=style.css>
+<div>
 <h1>Thanks for the suggestion!</h1>
 <a href=javascript:history.go(-1)>Go Back</a>
+</div>
