5f51d645474a14ac3780246b36299c5242d6626d
Chris McKenzie <kristopolous@yahoo.com>
2012-01-23 15:54:42 -0800

    fixes for archiving

--- volume/getdata.sh
+++ volume/getdata.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cat ../playlist.js | grep yt | awk -F \" ' { print $2 } ' | awk -F : ' { print $2 } ' > ytlist.dat
+cat ../js/playlist.js | grep yt | awk -F \" ' { print $2 } ' | awk -F : ' { print $2 } ' > ytlist.dat
 
 cd archive
 for i in `cat ../ytlist.dat`; do
   echo "<< Attempting $i >>"
-  [ -e $i.flv ] || ../youtube-dl.py --max-quality 5 $i
+  [ -e $i.mp3 ] || ../youtube-dl.py -c --extract-audio -f 5 -- $i
 done
