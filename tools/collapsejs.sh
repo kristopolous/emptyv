e4d95b297d752ecbeacf6480ae8dbe7d70c706d0
Chris McKenzie <kristopolous@yahoo.com>
2012-01-27 20:54:12 -0800

    more minification work

--- tools/collapsejs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ../js
-
-cat swfobject.js underscore-min.js _inject.min.js > collapsed.js
---
+++ tools/minifier.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd ../js
+in=all.js
+out=all.min.js
+
+cat swfobject.js underscore-min.js _inject.min.js playlist.js mt80s.js > all.js 
+curl -s \
+        -d compilation_level=SIMPLE_OPTIMIZATIONS \
+        -d output_format=text \
+        -d output_info=compiled_code \
+        --data-urlencode "js_code@${in}" \
+        http://closure-compiler.appspot.com/compile \
+        > $out
