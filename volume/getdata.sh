01271030a514d33cd5ac2b903d7efea04ff0687e
www-data <www-data@mail.iizuu.com>
2012-05-01 15:30:40 -0700

    playlist updates

--- volume/getdata.sh
+++ volume/getdata.sh
@@ -7,9 +7,10 @@
 # of ffmpeg through a make install.
 #
 cat ../js/playlist.js | grep yt | awk -F \" ' { print $2 } ' | awk -F : ' { print $2 } ' > ytlist.dat
+echo "hMVW8E5O3uA" >> ytlist.dat
 
 cd archive
 for i in `cat ../ytlist.dat`; do
   echo "<< Attempting $i >>"
-  [ -e $i.mp3 ] || ../../tools/youtube-dl.py -c --extract-audio -f 5 -- $i
+  [ -e $i.aac ] || ../../tools/youtube-dl.py -c --extract-audio -f 18 -- $i
 done
