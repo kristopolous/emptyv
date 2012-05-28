987cfb718ac2eef5779f32a51255f8d82dc8fe85
www-data <www-data@mail.iizuu.com>
2012-05-27 18:30:26 -0700

    dupe checker functional + daemonize scripts

---
+++ node/daemonize.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while true; do
+  node app.js
+done
--- node/dupe_checker.js
+++ node/dupe_checker.js
@@ -3,9 +3,17 @@ var redis = require('redis')
 
 _db.select(1);
 
-_db.hgetall("tick", function(err, state) {
-  for(var channel in state) {
-    
+function rebuild(channel, list) {
+  if(list.length > 0) {
+    console.log("Putting " + list[0] + " upon " + channel);
+    _db.rpush('pl:' + channel, list.shift(), function(err, last) {
+      rebuild(channel, list);
+    });
+  }
+}
+
+_db.hkeys("tick", function(err, state) {
+  state.forEach(function(channel) {
     _db.lrange("pl:" + channel, 0, -1, function(err, alltunes) {
       var 
         hash = {},
@@ -20,6 +28,9 @@ _db.hgetall("tick", function(err, state) {
         }
       });
       console.log("I want to modify " + channel + " by replacing " + alltunes.length + " with " + cleaned.length);
+      _db.del("pl:" + channel, function(err, last) {
+        rebuild(channel, cleaned);
+      });
     });
-  }
+  });
 });
