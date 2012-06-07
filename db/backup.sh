1b030ee868b74ec3d0a75252d0f4313dfd67d87d
www-data <www-data@mail.iizuu.com>
2012-06-07 12:13:15 -0700

    backup now running

--- db/backup.sh
+++ db/backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd /lvm/mt80s/db/
 cp dump.rdb dump.backup.rdb
 lzma -c dump.backup.rdb > dump.backup.rdb.lz
+scp dump.backup.rdb.lz mt80s@qaa.ath.cx:/raid/mt80s/db/redis-`date +"%Y%m%d-%H%M"`.rdb.lz
